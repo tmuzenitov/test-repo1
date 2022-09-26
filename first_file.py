58e638a889449cfb18c2df95789cf5eb985fc49d
tmuzenitov <tmuzenitov@gmail.com>
2022-09-26 20:07:54 +0400

    commit three

--- first_file.py
+++ first_file.py
@@ -1,3 +1,7 @@
 print('Hello GitHub')
 
 print("I think i'm getting hang of this")
+
+print("I think i'm getting hang of this")
+print("I think i'm getting hang of this")
+print("I think i'm getting hang of this")
